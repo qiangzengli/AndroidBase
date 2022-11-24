27728d4a90a70f93754f643d880c4f7726a67e02
李增强 <2224357370@qq.com>
2022-11-24 10:05:06 +0800

    🎨 优化toast 扩展由FragmentActivity->Activity,支持更大范围

--- base/src/main/java/zeng/qiang/base/extension/ToastExt.kt
+++ base/src/main/java/zeng/qiang/base/extension/ToastExt.kt
@@ -1,13 +1,13 @@
 package zeng.qiang.base.extension
 
+import android.app.Activity
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
 import com.blankj.utilcode.util.ToastUtils
 
 /**
- * FragmentActivity 扩展方法 toast
+ * Activity 扩展方法 toast
  */
-fun FragmentActivity.toast(msg: Any) = ToastUtils.showShort(msg.toString())
+fun Activity.toast(msg: Any) = ToastUtils.showShort(msg.toString())
 
 /**
  * Fragment 扩展方法 toast
