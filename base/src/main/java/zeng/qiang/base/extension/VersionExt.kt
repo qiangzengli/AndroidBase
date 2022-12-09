174ff9a5ede4a6fdc7a0a7c8d22d8a5c738426a7
李增强 <2224357370@qq.com>
2022-12-09 18:02:56 +0800

    ⬆️ 扩展属性指定精确类型

--- base/src/main/java/zeng/qiang/base/extension/VersionExt.kt
+++ base/src/main/java/zeng/qiang/base/extension/VersionExt.kt
@@ -1,17 +1,16 @@
 package zeng.qiang.base.extension
 
 import android.content.Context
+import android.content.pm.PackageInfo
 import android.os.Build
 
 
-val Context.packageInfo
+val Context.packageInfo: PackageInfo
     get() = packageManager.getPackageInfo(packageName, 0)
 
-val Context.versionName
+val Context.versionName: String
     get() = packageInfo.versionName
 
 val Context.versionCode: String
-    get() = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P)
-        packageInfo.longVersionCode.toString()
-    else
-        packageInfo.versionName
+    get() = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) packageInfo.longVersionCode.toString()
+    else packageInfo.versionName
