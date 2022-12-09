3040480d2d7d85e86f9e7871f637a0739b952d49
李增强 <2224357370@qq.com>
2022-12-09 17:58:49 +0800

    ⬆️ 兼容适配

--- base/src/main/java/zeng/qiang/base/extension/VersionExt.kt
+++ base/src/main/java/zeng/qiang/base/extension/VersionExt.kt
@@ -1,10 +1,17 @@
 package zeng.qiang.base.extension
 
 import android.content.Context
+import android.os.Build
 
 
 val Context.versionName
     get() = packageManager.getPackageInfo(packageName, 0).versionName
 
-val Context.versionCode
-    get() = packageManager.getPackageInfo(packageName, 0).versionCode
+val Context.versionCode: String
+    get() {
+        val packageInfo = packageManager.getPackageInfo(packageName, 0)
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P)
+            packageInfo.longVersionCode.toString()
+        else
+            packageInfo.versionName
+    }
